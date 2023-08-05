9beae2033099c126165095626439d03f5d07183a
Gerald Ooi <27208744+geraldooi@users.noreply.github.com>
2023-08-05 16:01:35 +0800

    Add content and styling to the page

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,21 @@
 export default function HomePage() {
-  return <p>Coming soon: a movie library app!</p>;
+  const appTitle = "My Movie Library";
+  const url = "https://www.themoviedb.org/";
+
+  return (
+    <main>
+      <h1>{appTitle.toUpperCase()}</h1>
+      <p>Here is a list of popular movies:</p>
+      <ul className="movie-list">
+        <li className="featured">Indiana Jones and the Dial of Destiny</li>
+        <li>Guardians of the Galaxy Vol. 3</li>
+        <li>Mission: Impossible - Dead Reckoning Part One</li>
+        <li>Spider-Man: Across the Spider-Verse</li>
+        <li>The Super Mario Bros. Movie</li>
+      </ul>
+      <p>
+        <a href="{url}">See more</a>
+      </p>
+    </main>
+  );
 }
